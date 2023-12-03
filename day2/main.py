672bd4df818d2f1345f0ec1331810099457fa9d7
Regis-1 <jdudzinski101@gmail.com>
2023-12-03 21:04:01 +0100

    day 2 - done

---
+++ day2/main.py
@@ -0,0 +1,28 @@
+import sys
+
+def evaluate_game(line: str, num_r: int, num_g: int, num_b: int):
+    result = True
+    splitted_game = line.split(' ')
+    id = int(splitted_game[1][:-1])
+    splitted_game = splitted_game[2:]
+    for i in range(0, len(splitted_game), 2):
+        if (('red' in splitted_game[i+1]) and (int(splitted_game[i]) > num_r)) or \
+        (('green' in splitted_game[i+1]) and (int(splitted_game[i]) > num_g)) or \
+        (('blue' in splitted_game[i+1]) and (int(splitted_game[i]) > num_b)):
+            result = False
+
+    return (id, result)
+
+def main():
+    if (len(sys.argv) <= 1):
+        return
+
+    with open(sys.argv[1], 'r') as my_file:
+        valid_games = []
+        for line in my_file:
+            valid_games.append(evaluate_game(line, 12, 13, 14))
+
+    valid_indexes = [x[0] for x in valid_games if x[1]]
+    print(sum(valid_indexes))
+
+main()
---
+++ day2/main2.py
@@ -0,0 +1,30 @@
+import sys
+
+def evaluate_game(line: str):
+    result = True
+    splitted_game = line.split(' ')
+    splitted_game = splitted_game[2:]
+    max_rgb = [0,0,0]
+    for i in range(0, len(splitted_game), 2):
+        if ('red' in splitted_game[i+1] and int(splitted_game[i]) > max_rgb[0]):
+            max_rgb[0] = int(splitted_game[i])
+        elif ('green' in splitted_game[i+1] and int(splitted_game[i]) > max_rgb[1]):
+            max_rgb[1] = int(splitted_game[i])
+        elif ('blue' in splitted_game[i+1] and int(splitted_game[i]) > max_rgb[2]):
+            max_rgb[2] = int(splitted_game[i])
+
+    return max_rgb
+
+def main():
+    if (len(sys.argv) <= 1):
+        return
+
+    with open(sys.argv[1], 'r') as my_file:
+        games_balls = []
+        for line in my_file:
+            games_balls.append(evaluate_game(line))
+
+    games_power = [x[0]*x[1]*x[2] for x in games_balls]
+    print(sum(games_power))
+
+main()
