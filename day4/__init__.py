f096d377f8485ff85eca13258db391bfd28deeb6
Regis-1 <jdudzinski101@gmail.com>
2023-12-09 19:18:48 +0100

    day 4 - done

--- day4/__init__.py
+++ day4/__init__.py
@@ -1,3 +1,5 @@
 from day4.main import day4_1
+from day4.main2 import day4_2
 
 day4_1('day4/inp_actual.txt')
+day4_2('day4/inp_actual.txt')
--- day4/main2.py
+++ day4/main2.py
@@ -1,3 +1,23 @@
+def extract_card_numbers(input):
+    cards_id = [int(line.split(':')[0].split(' ')[-1]) for line in input]
+    winning_numbers = []
+    have_numbers = []
+    for numbers in [line.split(':')[1] for line in input]:
+        winning_numbers.append(
+            [int(x) for x in numbers.split('|')[0].split(' ') if x != '']
+        )
+        have_numbers.append(
+            [int(x) for x in numbers.split('|')[1].split(' ') if x != '']
+        )
+    return (cards_id, winning_numbers, have_numbers)
+
+def count_scratchcards(numbers):
+    for cid in numbers[0]:
+        have_winnings = [n for n in numbers[2][cid-1] if n in numbers[1][cid-1]]
+        for i in range(len(have_winnings)):
+            numbers[0].append(cid+i+1)
+    return numbers[0]
+
 def read_input(file):
     input = []
     for line in file.readlines():
@@ -8,4 +28,7 @@ def day4_2(filename: str):
     with open(filename, 'r') as file:
         cards_input = read_input(file)
 
-day4_2('inp_test.txt')
+    cards_numbers = extract_card_numbers(cards_input)
+    all_scratchcards = count_scratchcards(cards_numbers)
+
+    print(f'The result of day4_2 is: {len(all_scratchcards)}.')
